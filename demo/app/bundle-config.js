82566ce1ca8d839372e94209a368009e443d8460
Andy <voxxxlll@gmail.com>
2021-06-01 12:49:38 +0100

    move from src to root

--- demo/app/bundle-config.js
+++
@@ -1,7 +0,0 @@
-if (global.TNS_WEBPACK) {
-    //registers @nativescript/core UI framework modules
-    require("bundle-entry-points");
-
-    //register application modules
-    global.registerModule("main-page", function () { return require("./main-page"); });
-}
